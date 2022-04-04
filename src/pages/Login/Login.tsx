a90dc62486a546fc1faef8d30e050840b2acb3e0
Diederik Eenschooten <diederikeen@gmail.com>
2022-04-04 19:07:09 +0200

    Make basic post request with react-hook-form

--- src/pages/Login/Login.tsx
+++ src/pages/Login/Login.tsx
@@ -1,5 +1,51 @@
+import { SubmitHandler, useForm } from "react-hook-form";
+import { yupResolver } from "@hookform/resolvers/yup";
+import * as yup from "yup";
+
+interface IFormInputs {
+  email: string;
+  password: string;
+}
+
+const schema = yup
+  .object({
+    email: yup.string().email().required(),
+    password: yup.string().min(8).required(),
+  })
+  .required();
+
 export function Login() {
+  const {
+    register,
+    handleSubmit,
+    formState: { errors },
+  } = useForm<IFormInputs>({
+    resolver: yupResolver(schema),
+  });
+
+  const onSubmit: SubmitHandler<IFormInputs> = (data) => {
+    return fetch("/api/user/login", {
+      method: "post",
+      body: JSON.stringify(data),
+      headers: { "Content-Type": "application/json" },
+    })
+      .then((res) => res.json())
+      .then((data) => console.log(data));
+  };
+
   return (
-    <p>You can login here</p>
-  )
+    <>
+      <form onSubmit={handleSubmit(onSubmit)}>
+        <input {...register("email")} />
+        <p>{errors.email?.message}</p>
+        <p>diederikeen@gmail.com</p>
+
+        <input {...register("password")} />
+        <p>{errors.password?.message}</p>
+        <p>mypassword</p>
+
+        <input type="submit" />
+      </form>
+    </>
+  );
 }
